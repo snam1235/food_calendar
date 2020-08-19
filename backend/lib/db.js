1d4c1ef4748b90cb7e18ddf030f494af19fa32b6
swon1235 <snam1235@terpmail.umd.edu>
2020-08-18 20:41:50 -0400

    deploying

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -4,7 +4,8 @@ module.exports.connect;
 //connect to mongoDB
 module.exports.connect = () =>
   mongoose.connect(
-    "mongodb+srv://ddoongss:sean1030@cluster0-8keyw.mongodb.net/test?retryWrites=true&w=majority",
+    process.env.MONGODB_URI ||
+      "mongodb+srv://ddoongss:sean1030@cluster0-8keyw.mongodb.net/test?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
     function (error) {
       if (error) console.log(error);
