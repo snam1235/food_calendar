427e6849053753ae72a37dffb4b5102fff21b3e8
swon1235 <snam1235@terpmail.umd.edu>
2020-08-20 19:17:54 -0400

    login message server side fix attempt

--- backend/routes/login_fail.js
+++ backend/routes/login_fail.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/login_fail_controller");
 
-router.get("/", controller.fail);
+router.get(
+  "/",
+  (res, req) => {
+    console.log(" here we at login fail route!");
+  },
+  controller.fail
+);
 
 module.exports = router;
